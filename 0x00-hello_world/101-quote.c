e8e5414a690bde3dbc0de861adefe2505ea2e49f
Hamid-Hussein <hamidhussein72@gmail.com>
2023-10-05 15:05:17 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-                                                  /**                                                * main - Entery point
+/**                                                * main - Entery point
  *                                                 * Return: Always 0 (Success)
  */
 int main(void)
