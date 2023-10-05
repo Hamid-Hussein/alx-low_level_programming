b7c9e3193de62c6143e7f44e611315ed9b597cb9
Hamid-Hussein <hamidhussein72@gmail.com>
2023-10-05 14:38:37 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-                                                  /**                                                * main - Entery point
+/**                                                * main - Entery point
  *                                                 * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
