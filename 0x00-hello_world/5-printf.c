c46973c0314940ec5a2b7fcbba41f127b7edaf6d
Hamid-Hussein <hamidhussein72@gmail.com>
2023-10-08 04:51:41 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-                                                  /**                                                * main - Entery point
- *                                                 * Return: Always 0 (Success)
+/**
+ * main - main function
+ *
+ * Return: always 0
  */
 
 int main(void)
