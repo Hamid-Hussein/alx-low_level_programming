1b97e73bcf3c2e27bc4aa71e9767687fb9898a87
Hamid-Hussein <hamidhussein72@gmail.com>
2023-10-08 05:01:50 -0700

    qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,10 @@
 #include <unistd.h>
-/**                                                * main - Entery point
- *                                                 * Return: Always 0 (Success)
+/**
+ * main - main function
+ *
+ * Return: always 0 (success)
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
