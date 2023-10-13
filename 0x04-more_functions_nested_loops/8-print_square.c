f73bf3bd5f9ebc45e199f5ad771efed1218baf22
Hamid-Hussein <hamidhussein72@gmail.com>
2023-10-13 06:27:39 -0700

    more_numbers

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,13 +3,13 @@
 /**
  * print_square - prints a square of size size
  * @size: size of the square
- *
  * Return: void
  */
 
 void print_square(int size)
 {
-	int i, j;
+	int i;
+	int j;
 
 	if (size <= 0)
 		_putchar('\n');
@@ -18,7 +18,7 @@ void print_square(int size)
 	{
 		for (j = 0; j < (size); j++)
 		{
-			_putchar('#");
+			_putchar('#');
 		}
 		_putchar('\n');
 	}
