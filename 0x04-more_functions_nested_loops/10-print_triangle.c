121f60b9ecebb8cc5c839981c35f9bbc1b463c84
Hamid-Hussein <hamidhussein72@gmail.com>
2023-10-13 05:44:32 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_triangle - entry point
+ * Description: print diagonals
+ * @size: size of the triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+
+}
