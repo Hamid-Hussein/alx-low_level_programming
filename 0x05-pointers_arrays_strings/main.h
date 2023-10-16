38c312edea649ed9fbb5450f56289df3d4853a6c
Hamid-Hussein <hamidhussein72@gmail.com>
2023-10-16 13:45:43 -0700

    puts2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -8,6 +9,7 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void puts2(char *str);
 
 
 #endif
