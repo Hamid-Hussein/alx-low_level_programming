bae819d2fb6bf8449ca26a2add350bde9bb25f88
Hamid-Hussein <hamidhussein72@gmail.com>
2023-10-20 05:29:46 -0700

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,13 +15,13 @@ char *cap_string(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s[i] = s[i] - 32;
+			s[i] -= 32;
 		for (j = 0; j < 13; j++)
 		{
 			if (s[i] == sp[j])
 			{
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-					s[i + 1] = s[i + 1] - 32;
+					s[i + 1] -= 32;
 			}
 		}
 	}
