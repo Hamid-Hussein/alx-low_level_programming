406a3e5cbb851bb3a832edd0c77f6561ad7ed5b2
Hamid-Hussein <hamidhussein72@gmail.com>
2023-10-28 06:13:14 -0700

    libmy.a, main.h

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,16 +1,1 @@
-#include "main.h"
 
-/**
- * _atoi - convert string to an integer
- * @s: string to be converted
- * Return: 0
- */
-
-int _atoi(char *s)
-{
-	s = "hello world";
-	int i = atoi(s);
-
-	printf("integer: %d\n", i);
-	return (0);
-}
