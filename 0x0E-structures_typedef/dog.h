45b64d87d9ac5b677bd15f511af465f79dc23652
Hamid-Hussein <hamidhussein72@gmail.com>
2023-11-07 12:07:54 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef STRUCT_DOG_H
+#define STRUCT_DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
