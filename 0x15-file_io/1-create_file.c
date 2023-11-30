1c685fcc5083ace5a8fee5afb4080e8f096fdf53
Hamid-Hussein <hamidhussein72@gmail.com>
2023-11-30 12:15:06 -0800

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,10 +28,10 @@ int _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t byzes = 0, len = _strlen(text_content);
+	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
-		rerurn (-1);
+		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IWUSR);
 	if (fd == -1)
 		return (-1);
